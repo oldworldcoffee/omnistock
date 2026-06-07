ea8d4168044b798ab01b5db75dda8c789f4e70c8
oldworldcoffee <chris@oldworldcoffeeroasters.com>
2026-06-06 23:40:23 -0700

    api fixes

---
+++ api/functions/[...path].js
@@ -0,0 +1,11 @@
+import { requestHandler } from '../../server/server.js';
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+export default function handler(req, res) {
+  return requestHandler(req, res);
+}
